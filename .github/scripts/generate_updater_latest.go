5017701bda9fde1a0371c0cc00bbbbd4749e9160
Tori <you@example.com>
2026-09-10 14:32:28 -0700

    Add release build workflows

--- .github/scripts/generate_updater_latest.go
+++ .github/scripts/generate_updater_latest.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	DownloadUrl = "https://github.com/5rahim/seanime/releases/latest/download/"
+	DownloadUrl = "https://github.com/yeonvvx/tori/releases/latest/download/"
 )
 
 func main() {
