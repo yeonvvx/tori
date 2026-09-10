4b02c27b1aa5d0daeda445efe02ce4956bbd3fbe
Tori <you@example.com>
2026-09-10 15:04:38 -0700

    Point extension marketplace and announcements at yeonvvx/tori-extensions

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -21,6 +21,6 @@ const (
 	SeanimeRoomsVersion  = "1.0.0"
 )
 
-var DefaultExtensionMarketplaceURL = util.Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tLzVyYWhpbS9zZWFuaW1lLWV4dGVuc2lvbnMvcmVmcy9oZWFkcy9tYWluL21hcmtldHBsYWNlLmpzb24=")
-var AnnouncementURL = util.Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tLzVyYWhpbS9oaWJpa2UvcmVmcy9oZWFkcy9tYWluL3B1YmxpYy9hbm5vdW5jZW1lbnRzLmpzb24=")
+var DefaultExtensionMarketplaceURL = util.Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3llb252dngvdG9yaS1leHRlbnNpb25zL3JlZnMvaGVhZHMvbWFpbi9tYXJrZXRwbGFjZS5qc29u")
+var AnnouncementURL = util.Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3llb252dngvdG9yaS1leHRlbnNpb25zL3JlZnMvaGVhZHMvbWFpbi9hbm5vdW5jZW1lbnRzLmpzb24=")
 var InternalMetadataURL = util.Decode("aHR0cHM6Ly9hbmltZS5jbGFwLmluZw==")
